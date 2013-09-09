63b2b98e4192dc8d6e2a09217f166ad3408a01b1
unknown <n02076294@.active.newpaltz.edu>
2013-09-09 16:16:22 -0400

    class work

--- final/index.php
+++ final/index.php
@@ -1,2 +1,24 @@
-hello this is the final project
-which is going to be e-commerce site
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
+ 
+   
+  </head>
+  <body>
+    <h1>This the final</h1>
+    
+    <? 
+    	$msg = 'Hello World ' ;
+		$name = "Luisa";
+		include 'sometthing.php';
+    ?>
+    
+    <spam class= "label label-success"> <? echo $msg . $name ?></spam>
+ 	<script src="//code.jquery.com/jquery.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"></script>
+  </body>
+</html>
--- playground/index.php
+++ playground/index.php
@@ -1,51 +1,106 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Bootstrap 101 Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+    <title>Jumbotron Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+
+     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="../../assets/js/html5shiv.js"></script>
       <script src="../../assets/js/respond.min.js"></script>
     <![endif]-->
-    <style>
-    	.row div {
-    		background-color: #B8B8B8 ;
-    		
-    	}
-    </style>
   </head>
+
   <body>
-    <h1>This the playground</h1>
 
-    
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Playground</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#contact">Contact</a></li>
+             <li><a href="#link">Links</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            Signed in as Luisa Martinez
+          </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </div>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Hello world!</h1>
+        <p> Welcome class of 2013 to Web Server Programming</p>
+        <p><a class="btn btn-success">Learn more </a></p>
+      </div>
+    </div>
+
     <div class="container">
-		    <div class="row">
-		  <div class="col-xs-12 col-md-8">.col-xs-12 col-md-8</div>
-		  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-		</div>
-		
-		<!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
-		<div class="row">
-		  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-		  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-		  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-		</div>
-		
-		<!-- Columns are always 50% wide, on mobile and desktop -->
-		<div class="row">
-		  <div class="col-xs-6">.col-xs-6</div>
-		  <div class="col-xs-6">.col-xs-6</div>
-		</div>
-	</div>	    
-    
- 
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-lg-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn  btn-primary btn-lg " href="#">View details &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+          <h2>Important Points</h2>
+          <li>The three main links in the navbar work</li>
+          <li>They are all centralized in one file/li>
+          <li>They change apearance to show you which page you are on.</li>
+          <li>These colunms start as three columns then reduce as the browser shrinks</li>
+          <p><a class="btn  btn-primary btn-lg " href="#">View details &raquo; </a></p>
+       </div>
+        <div class="col-lg-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn  btn-primary btn-lg " href="#">View details &raquo;</a></p>
+        </div>
+      </div>
 
-    <script src="//code.jquery.com/jquery.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"></script>
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2013</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../../assets/js/jquery.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
   </body>
 </html>
+
+		
+
+
 
