88bfad31773c7d32a61ba97dd4eb0568d683e045
unknown <n02076294@.active.newpaltz.edu>
2013-10-21 15:40:53 -0400

    class

--- final/Views/Users/new.php
+++ final/Views/Users/new.php
@@ -2,11 +2,34 @@
         <form action="?action=save" method="post" class="form-horizontal">
                 <div class="form-group">
                         <label for="FistName">First Name</label>
-                        <input type="text" name="FirstName" id="FirstName" placeholder="First Name" class="form-control" />
+                        <div class="class="col-sm-10 ">
+                        	<input type="text" name="FirstName" id="FirstName" placeholder="First Name" class="form-control" />
+                        </div>
+                        
                 </div>
                 <div class="form-group">
+                        <label for="FistName">Last Name</label>
+                        <div class="class="col-sm-10 ">
+                        	<input type="text" name="LastName" id="LastName" placeholder="Last Name" class="form-control" />
+                        </div>
                         
-                        <input type="submit" class="form-control" value="Save" />
                 </div>
+                 <div class="form-group">
+                        <label for="FistName">Password </label>
+                        <div class="class="col-sm-10 ">
+                        	<input type="password " name="Password " id="Password " placeholder="Password " class="form-control" />
+                        </div>
+                        
+                </div>
+                 <div class="form-group">
+                        <label for="FistName">UserType </label>
+                        <div class="class="col-sm-10 ">
+                        	<input type="text" name="UserType " id="UserType " placeholder="UserType " class="form-control" />
+                        </div>
+                        
+                </div>
+                
+               
+               
         </form>
 </div>
