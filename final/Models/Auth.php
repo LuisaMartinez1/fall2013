1bdb6fc70522dd3e508e96fe77ef8062ff8495ec
LuisaMartinez <fernanda_14137@hotmail.com>
2013-12-09 16:20:37 -0500

    LoginSystem

---
+++ final/Models/Auth.php
@@ -0,0 +1,38 @@
+<?
+const ADMIN = 7;
+	class Auth
+	{
+		public static function IsLoggedIn()
+		{
+			return self::GetUser() != null;
+		}
+		public static function GetUser()
+		{
+			return $_SESSION['User'];
+		}
+		public static function HasPermission()
+		{
+			$user = self::GetUser();
+			return $user['KeyWords_id'] ==  ADMIN;
+			
+		}
+		public static function LogIn($userName, $password)
+		{
+			$sql = " SELECT  U.* 
+                	 FROM Fall2013_Users U 
+                        	
+                      WHERE U.LastName = $userName";
+                      $user =  fetch_one($sql);   
+			if($user['password'] == $password)
+			{
+				$_SESSION['User'] = $user;
+			}    
+		}
+		static public function Secure()
+		{
+			if(!self::IsLoggedIn())
+			{
+				header("Location " .   "/~n02076294/2013/final/../Views/Auth?action=login"); die();
+			}
+		}
+	}
---
+++ final/Views/Auth/login.php
@@ -0,0 +1,44 @@
+<style type = "text/css">
+    .error{color:red;}
+</style>
+<div class="container">
+	<? if(isset($errors) && $errors): ?>
+	<ul class="error">
+		<? foreach ($errors as $key => $value): ?>
+			<li>
+				<label><?=$key?>:</label><?=$value?>
+			</li>
+		<? endforeach; ?>
+	</ul>
+	<? endif; ?>
+        <form action="?action=submitlogin" method="post" class="form-horizontal row">
+             
+                
+                
+                <div class="form-group <?=isset($errors['LastName']) ? 'has error' : '' ?>">
+                        <label for="LastName" class="col-sm-2 control-label">Last Name</label>
+                        <div class="col-sm-10">
+                                <input type="text" name="LastName" id="LastName" placeholder="Last Name" class="form-control" value="<?=$model['LastName']?>"/>
+                                <? if(isset($errors['LastName'])): ?><span class = "error"><?=$errors['LastName'] ?> </span> <? endif;?>
+                        </div>
+                </div>
+                <div class="form-group <?=isset($errors['password']) ? 'has error' : '' ?>">
+                        <label for="password" class="col-sm-2 control-label">Password</label>
+                        <div class="col-sm-10">
+                                <input type="password" name="password" id="password" placeholder="password" class="form-control" value="<?=$model['password']?>"/>
+                                <? if(isset($errors['password'])): ?><span class = "error"><?=$errors['password'] ?> </span> <? endif;?>
+                        </div>
+                </div>
+              
+                <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                                <input type="submit" class="form-control btn btn-primary" value="Login"/>
+                        </div>
+                </div>
+        </form>
+</div>
+<script type="text/javascript">
+        $(function(){
+                $("#KeyWords_id").val(<?=$model['KeyWords_id']?>);
+        })
+</script>
